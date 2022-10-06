c470bbdd0cf6dc4390514b4606437fa2145a2109
LNKNITE <ashishkr.xn@gmail.com>
2022-10-06 16:48:20 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const readlineSync = require('readline-sync')
+const request = require('sync-request')
+
+let username = readlineSync.question("Enter Your Username -> \n")
+console.log(`Type "Exit" to End Conversation.`)
+
+while (true) {
+    let command = readlineSync.prompt()
+    if (command.toUpperCase() == "EXIT") {
+        break
+    }
+    else {
+        let api_url = `http://api.brainshop.ai/get?bid=${process.env.BRAINSHOP_BID}&key=${process.env.BRAINSHOP_KEY}&uid=${username}&msg=${command}`
+        let res = request('GET', api_url)
+        let data = JSON.parse(res.getBody().toString())
+        console.log(data.cnt)
+    }
+}
